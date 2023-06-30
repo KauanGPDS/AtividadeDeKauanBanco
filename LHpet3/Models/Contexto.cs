49ccb3bfe742a98b26d32eb06fd1311ccbb3a5c3
KauanGPDS <107084933+KauanGPDS@users.noreply.github.com>
2023-06-30 19:19:26 -0300

    k

---
+++ LHpet3/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHpet3.Models
+{
+  
+        public class Contexto : DbContext
+        {
+            public Contexto(DbContextOptions<Contexto> options) : base(options)
+            {
+            }
+            public DbSet<Cliente> Cliente { get; set; }
+        }
+    
+}
---
+++ LHpet3/Program.cs
@@ -0,0 +1,30 @@
+
+using Microsoft.EntityFrameworkCore;
+using LHpet3.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+(options => options.UseSqlServer("DESKTOP-FKD2LT7\\SQLEXPRESS;Initial Catalog = LHPet; Trusted_Connection = True; "));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
